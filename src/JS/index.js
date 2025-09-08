5c15bb8f102f4c16475bbd4beac20d6b21cc8516
Pankaj Bepari <pankajbepari7@gmail.com>
2025-09-08 20:37:58 +0600

    add all catagories

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,1 +1,28 @@
-console.log("JS is linked");
+// All Categories API URL
+const categoriesApiURL = "https://openapi.programming-hero.com/api/categories";
+
+// Fetch to get data
+fetch(categoriesApiURL)
+  .then((response) => response.json()) // Convert the response to JSON
+  .then((data) => {
+    // call displayCategories with data
+    displayCategories(data.categories);
+  });
+
+// Function to display the categories on the page
+const displayCategories = (categories) => {
+  const categoriesContainer = document.getElementById("categories-container");
+
+  // Initialize an empty string to store the HTML
+  let buttonsHTML = "";
+
+  // Loop through each category and add to the string
+  categories.forEach((category) => {
+    buttonsHTML += `
+      <button class="btn btn-ghost w-full justify-start">${category.category_name}</button>
+    `;
+  });
+
+  // Use .innerHTML to set the content from the string
+  categoriesContainer.innerHTML = buttonsHTML;
+};
